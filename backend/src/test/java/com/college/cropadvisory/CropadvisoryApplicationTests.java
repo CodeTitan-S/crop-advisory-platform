085617e106173f61885a427ee78a8826a3de2b33
CodeTitan-S <171786228+CodeTitan-S@users.noreply.github.com>
2026-08-10 11:38:06 +0530

    Changed the backend from python to java envirnonment and scaffolded the Spring boot

---
+++ backend/src/test/java/com/college/cropadvisory/CropadvisoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.college.cropadvisory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CropadvisoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
